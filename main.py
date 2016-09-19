fa2c86b4817623fdac32d61944383ea190f24295
YongliuLi <ylli@thoughtworks.com>
2016-09-20 00:36:23 +0800

    [#0] add main file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python
+import pika
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'rabbitmq.dev.twleansw.com'))
+channel = connection.channel()
+channel.queue_declare(queue='remote_power_switch')
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    if body == 'Hello World!' :
+    	print('Got Hello World!')
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
