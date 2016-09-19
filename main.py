e91b41a2a8b5fffabfcace5a1dff7b9645b6dd8d
YongliuLi <ylli@thoughtworks.com>
2016-09-20 00:49:07 +0800

    [#0] add switch command

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import pika
+from  gpiozero import DigitalOutputDevice
+
+switch =  DigitalOutputDevice(21)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
                'rabbitmq.dev.twleansw.com'))
@@ -7,11 +10,13 @@ channel = connection.channel()
 channel.queue_declare(queue='remote_power_switch')
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    if body == 'Hello World!' :
-    	print('Got Hello World!')
+    if body == 'POWER_OFF' :
+    	switch.on()
+    if body === 'POWER_ON' :
+    	switch.off()
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue='remote_power_switch',
                       no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
---
+++ test_mq_send_switch_command.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pika
+import time
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'rabbitmq.dev.twleansw.com'))
+channel = connection.channel()
+for i in 0 , 3 :
+	channel.queue_declare(queue='remote_power_switch')
+	channel.basic_publish(exchange='',
+	                      routing_key='remote_power_switch',
+	                      body='POWER_ON')
+	print(" [x] Sent POWER_ON")
+	time.sleep(5) # delays for 5 seconds
+	
+	channel.queue_declare(queue='remote_power_switch')
+	channel.basic_publish(exchange='',
+	                      routing_key='remote_power_switch',
+	                      body='POWER_OFF')
+	time.sleep(5) # delays for 5 seconds
+	print(" [x] Sent POWER_OFF")
+
